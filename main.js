cd30a41ab71c65c67e38a37df5621eb7f62bbf2f
Romário Lima <devromariolima@gmail.com>
2023-10-02 16:46:59 -0300

    ajustes front

--- main.js
+++ main.js
@@ -1,37 +1,62 @@
 const repositories = document.querySelector('.content-main');
 
 function getApiGitHub(){
-fetch('https://api.github.com/users/Romariolima99/repos')
-.then(async res => {
-if ( !res.ok){
-    throw new Error(res.status);
+  let User = document.getElementById("User").value;
 
-}
-let data = await res.json();
-data.map(item =>{
-
-    let project = document.createElement('div');
-
-    project.innerHTML = `
-
-    <div class="project">
-    <div>
-  <h4 class="title">${item.name}</h4>
-  <span class="date-create">${Intl.DateTimeFormat('pt-br').format (new Date(item.created_at)) }</span>
-     </div>
-      <div>
-    <a id="url" href="${item.html_url}" target="_blank">${item.html_url}</a>
-    <span class="language"><span class="circle"></span>${item.language}</span>
- </div>
-</div> 
-`
-
-repositories.appendChild(project);
+  console.log("User", User);
   
+  fetch(`https://api.github.com/users/${User}/repos`)
+  .then(async res => {
+  if ( !res.ok){
+    alert("Usuario não encontrado")
+      throw new Error(res.status);
+      
+  }
 
-})
+  let data = await res.json();
 
-} )
+  if( !data || data.length <= 0 ){
+    alert("repositorios não encontrados");
+    return;
+  }
+
+  data.map(item => {
+    console.log("Recebendo", item.language);
+    if(item?.language?.length > 0){
+      let project = document.createElement('div');
+
+      project.innerHTML = `
+  
+      <div class="project">
+      <div>
+    <h4 class="title">${item.name}</h4>
+    <span class="date-create">${Intl.DateTimeFormat('pt-br').format (new Date(item.created_at)) }</span>
+      </div>
+        <div>
+      <a id="url" href="${item.html_url}" target="_blank">${item.html_url}</a>
+      <span class="language"><span class="circle"></span>${item.language}</span>
+  </div>
+  </div> 
+  `
+    repositories.appendChild(project);  
+    }
+  });
+
+  } )
 }
 
-getApiGitHub();
+function limpar (){
+  const  list = document.getElementById("limpar");
+  list.replaceChildren();
+}
+
+  const container = document.getElementById('limpar');
+  const btn = document.getElementById('btn');
+
+  btn.addEventListener('click',function handleClick(){
+    container.replaceChildren();
+  });
+
+
+
+//getApiGitHub();
